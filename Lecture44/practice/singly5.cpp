d2f92d7e440140860bf981dd120cb888afadd33e
Kartikey Mishra <kartikeymishra700@gmail.com>
2024-05-21 15:23:48 +0530

    changes

---
+++ Lecture44/practice/singly5.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;    // pointer to next node in the list.
+
+    // constructor
+    Node(int d)
+    {
+        this -> data = d;
+        this -> next = NULL;
+    }
+
+};
+
+void insertAthead(Node* &head, int d)
+{
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertAttail(Node* &tail, int d)
+{
+    Node* temp = new Node(d);
+    tail ->next = temp;
+    tail = temp;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp -> data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+int main()
+{
+
+    Node *n1 = new Node (5);
+    // Node *tail = head;
+
+    Node* head = n1;
+    Node* tail = n1;
+    print(head);
+    
+    insertAthead(head, 10);
+    print(head);
+
+    insertAthead(head, 12);
+    print(head);
+
+    // Node* tail = n1;
+
+    insertAttail(tail, 13);
+    print(tail);
+
+    return 0;
+}
